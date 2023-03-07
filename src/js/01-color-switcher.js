720ae370be5a5945321b35c89e05e4ed70af958a
OlegBilas <olegbilas@gmail.com>
2023-03-07 11:09:45 +0200

    завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+let intervalId;
 
+stoptBtn.setAttribute('disabled', 'true');
+
+startBtn.addEventListener('click', onClikChangeColor);
+stoptBtn.addEventListener('click', onClikStopChangeColor);
+
+function onClikChangeColor() {
+  if (startBtn.getAttribute('disabled')) {
+    return;
+  }
+  startBtn.setAttribute('disabled', 'true');
+  stoptBtn.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClikStopChangeColor() {
+  if (!startBtn.getAttribute('disabled')) {
+    return;
+  }
+  startBtn.removeAttribute('disabled');
+  stoptBtn.setAttribute('disabled', 'true');
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
