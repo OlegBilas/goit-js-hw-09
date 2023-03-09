194a447c1c53547a504a9065f62156cfaa293f09
OlegBilas <olegbilas@gmail.com>
2023-03-09 10:57:32 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const formRef = document.querySelector('.form');
+const {
+  elements: { delay: delayRef, step: stepRef, amount: amountRef },
+} = formRef;
+
+formRef.addEventListener('submit', onSubmitForm);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new PromiseRejectionEvent((resolve, reject) => {
+    if (shouldResolve) {
+      // Fulfill
+      resolve({ position, delay });
+    } else {
+      // Reject
+      reject({ position, delay });
+    }
+  });
+}
+
+function onSubmitForm(e) {
+  e.prevantDefault();
+  let position = 1;
+  let delay = Number(delayRef.value);
+  const amount = Number(amountRef.value);
+  while (position < amount) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += Number(stepRef.value);
+    counter += 1;
   }
+
+  e.currentTarget.reset();
 }
